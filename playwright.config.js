7bcef525955d08ab52a8077d7c01c488af182d9b
Emilie Sofie Fosmo <emilie.fosmo@hotmail.com>
2026-03-11 16:57:50 +0100

    added comment to playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -27,6 +27,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
     url: "http://localhost:5500",
